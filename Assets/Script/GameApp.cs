09e2a5609d6460f9c3713909eb7a3c599c35b5e8
junfff <james922@163.com>
2018-01-15 14:12:53 +0800

    Add files via upload

---
+++ Assets/Script/GameApp.cs
@@ -0,0 +1,164 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Script;
+
+public class GameApp : MonoBehaviour
+{
+    public Transform targetA;
+    //用于绑定参照物对象
+    public Transform target;
+    //缩放系数
+    float distance = 10;
+    //左右滑动移动速度
+    float xSpeed = 25;
+    float ySpeed = 12;
+    //缩放限制系数
+    float yMinLimit = -20;
+    float yMaxLimit = 120;
+    //摄像头的位置
+    float x = 0;
+    float y = 0;
+    //记录上一次手机触摸位置判断用户是在左放大还是缩小手势
+    private Vector2 oldPosition1;
+    private Vector2 oldPosition2;
+
+    /// <summary>  
+    /// 与y轴最小夹角  
+    /// </summary>  
+    public float yAxisMinAngle = 30;
+
+    /// <summary>  
+    /// 与y轴最大夹角  
+    /// </summary>  
+    public float yAxisMaxAngle = 330;
+    //初始化游戏信息设置
+    void Start()
+    {
+        var angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        // Make the rigid body not change rotation
+        //if (rigidbody)
+        //    rigidbody.freezeRotation = true;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+
+            x += Input.GetAxis("Mouse X") * xSpeed;
+            y -= Input.GetAxis("Mouse Y") * ySpeed;
+
+            y = ClampAngle(y, yAxisMinAngle, yAxisMaxAngle);
+
+            //Quaternion rotateAngle = Quaternion.Euler(y, x, 0);//摄像机偏转角度  
+
+            //Vector3 direction = new Vector3(0, yMaxLimit, -distance);//摄像机距离物品的距离  
+            //transform.rotation = rotateAngle;//让摄像机始终转向物品  
+            //transform.position = target.position + rotateAngle * direction;//计算旋转多少角度摄像机需要偏移多少  
+                                                                           //transform.LookAt(target);  
+
+            y = ClampAngle(y, yAxisMinAngle, yAxisMaxAngle);
+
+        }
+
+
+        //判断触摸数量为单点触摸
+        if (Input.touchCount == 1)
+        {
+            //触摸类型为移动触摸
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                //根据触摸点计算X与Y位置
+                x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+                y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            }
+        }
+
+        //判断触摸数量为多点触摸
+        if (Input.touchCount > 1)
+        {
+            //前两只手指触摸类型都为移动触摸
+            if (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Moved)
+            {
+                //计算出当前两点触摸点的位置
+                var tempPosition1 = Input.GetTouch(0).position;
+                var tempPosition2 = Input.GetTouch(1).position;
+                //函数返回真为放大，返回假为缩小
+                if (isEnlarge(oldPosition1, oldPosition2, tempPosition1, tempPosition2))
+                {
+                    //放大系数超过3以后不允许继续放大
+                    //这里的数据是根据我项目中的模型而调节的，大家可以自己任意修改
+                    if (distance > 3)
+                    {
+                        distance -= 0.5f;
+                    }
+                }
+                else
+                {
+                    //缩小洗漱返回18.5后不允许继续缩小
+                    //这里的数据是根据我项目中的模型而调节的，大家可以自己任意修改
+                    if (distance < 18.5)
+                    {
+                        distance += 0.5f;
+                    }
+                }
+                //备份上一次触摸点的位置，用于对比
+                oldPosition1 = tempPosition1;
+                oldPosition2 = tempPosition2;
+            }
+        }
+    }
+
+    //函数返回真为放大，返回假为缩小
+    bool isEnlarge(Vector2 oP1, Vector2 oP2, Vector2 nP1, Vector2 nP2)
+    {
+        //函数传入上一次触摸两点的位置与本次触摸两点的位置计算出用户的手势
+        var leng1 = Mathf.Sqrt((oP1.x - oP2.x) * (oP1.x - oP2.x) + (oP1.y - oP2.y) * (oP1.y - oP2.y));
+        var leng2 = Mathf.Sqrt((nP1.x - nP2.x) * (nP1.x - nP2.x) + (nP1.y - nP2.y) * (nP1.y - nP2.y));
+        if (leng1 < leng2)
+        {
+            //放大手势
+            return true;
+        }
+        else
+        {
+            //缩小手势
+            return false;
+        }
+    }
+
+    //Update方法一旦调用结束以后进入这里算出重置摄像机的位置
+    void LateUpdate()
+    {
+
+        //target为我们绑定的箱子变量，缩放旋转的参照物
+        if (target)
+        {
+
+            //重置摄像机的位置
+            y = ClampAngle((float)y, yMinLimit, yMaxLimit);
+            var rotation = Quaternion.Euler(y, x, 0);
+            var position = rotation *new Vector3(0, 0, -distance) + target.position;
+
+            //targetA.transform.rotation = rotation;
+            //targetA.transform.position = position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    static float ClampAngle(float angle  , float min  , float max  )
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/Script/MyWriter.cs
@@ -0,0 +1,256 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Assets.Script
+{
+
+    public class MyPtr
+    {
+        public IntPtr myP;
+        public int MyLength;
+    }
+    public class MyWriter
+    {
+        [DllImport("MyWriteHandler")]
+        private static extern int SaveBuf(IntPtr buf, int length);
+        [DllImport("MyWriteHandler")]
+        private static extern int GetBuf(out IntPtr unmanaged_ptr);
+        [DllImport("MyWriteHandler")]
+        private static extern int Add(int x, int y);
+
+        private const string path = @"C:\LJF\MyTestWriter.txt";
+
+        private FileInfo finfo;
+
+        private byte[] content;
+
+        private FileInfo file_info;
+        internal void OnStart()
+        {
+            file_info = new FileInfo(path);
+
+            string str = "今天星期天啊,等下吃饭了今天星期天啊,等下吃饭了今天星期天啊,等下吃饭了 !!";
+            content = System.Text.Encoding.UTF8.GetBytes(str);
+            OnWrite();
+        }
+
+        private int writeNum = 2;
+        private int curWrite;
+
+        List<byte[]> list = new List<byte[]>();
+
+        bool finish;
+        bool saveFinish;
+        internal void OnUpdate()
+        {
+            //TestAAAA();
+            Save4096();
+        }
+
+        private void Save4096()
+        {
+            content = new byte[40960];
+
+            IntPtr unmanaged_data_prt = Marshal.AllocHGlobal(content.Length);// 直接分配100 byte的内存
+            Marshal.Copy(content, 0, unmanaged_data_prt, content.Length);
+
+            MyPtr myP = new MyPtr();
+            myP.myP = unmanaged_data_prt;
+            myP.MyLength = content.Length;
+            listPtr.Add(myP);
+            //Marshal.FreeHGlobal(unmanaged_data_prt); //使用后销毁非托管内存
+
+
+            //GCHandle unmanaged_data_handle = GCHandle.Alloc(content, GCHandleType.Pinned); //这里将标记_managed_data暂时不能被gc回收，并且固定对象的地址
+            //int ret = SaveBuf(unmanaged_data_handle.AddrOfPinnedObject(), content.Length);//这里将拿到非托管内存的固定地址，传给c++
+            //unmanaged_data_handle.Free();//使用完毕后，将其handle free，这样c#可以正常gc这块内存
+
+            curWrite++;
+            UnityEngine.Debug.LogFormat("Savebuff curWrite = {0}  ret = {1}", curWrite, listPtr);
+        }
+
+        private void TestAAAA()
+        {
+            if (finish)
+            {
+                UnityEngine.Debug.LogFormat("save and write done !!");
+                return;
+            }
+            if (curWrite >= 100)
+            {
+                saveFinish = true;
+                while (true)
+                {
+                    int ret = UpWrite();
+                    if (ret == -1)
+                    {
+                        finish = true;
+                        return;
+                    }
+                }
+            }
+            if (saveFinish)
+            {
+                return;
+            }
+            Savebuff();
+        }
+
+        List<MyPtr> listPtr = new List<MyPtr>();
+        private void Savebuff()
+        {
+            //content = new byte[40960];
+
+            string str = "今天星期天啊,等下吃饭了今天星期天啊,等下吃饭了今天星期天啊,等下吃饭了 !!";
+            str = string.Format("{0}, num = {1}", str, curWrite);
+            content = System.Text.Encoding.UTF8.GetBytes(str);
+
+            IntPtr unmanaged_data_prt = Marshal.AllocHGlobal(content.Length);// 直接分配100 byte的内存
+            Marshal.Copy(content, 0, unmanaged_data_prt, content.Length);
+
+            MyPtr myP = new MyPtr();
+            myP.myP = unmanaged_data_prt;
+            myP.MyLength = content.Length;
+            listPtr.Add(myP);
+            //Marshal.FreeHGlobal(unmanaged_data_prt); //使用后销毁非托管内存
+
+
+            //GCHandle unmanaged_data_handle = GCHandle.Alloc(content, GCHandleType.Pinned); //这里将标记_managed_data暂时不能被gc回收，并且固定对象的地址
+            //int ret = SaveBuf(unmanaged_data_handle.AddrOfPinnedObject(), content.Length);//这里将拿到非托管内存的固定地址，传给c++
+            //unmanaged_data_handle.Free();//使用完毕后，将其handle free，这样c#可以正常gc这块内存
+
+            curWrite++;
+            UnityEngine.Debug.LogFormat("Savebuff curWrite = {0}  ret = {1}", curWrite, listPtr);
+        }
+
+        private int UpWrite()
+        {
+            IntPtr unmanaged_ptr = IntPtr.Zero; //定义这个c#中用来接收c++返回数据的指针类型
+            int length = GetPtr(ref unmanaged_ptr);//调用c++的函数，使unmanaged_ptr指向c++里分配的内存，注意这里用out ，才能与c++里面的**匹配。
+            if (length == -1)
+            {
+                return -1;
+            }
+            byte[] managed_data = new byte[length];
+            Marshal.Copy(unmanaged_ptr, managed_data, 0, length);//将非托管内存拷贝成托管内存，才能在c#里面使用
+            Marshal.FreeHGlobal(unmanaged_ptr);//释放非托管的内存
+
+
+      
+            WriterFile(managed_data);
+            curWrite--;
+            UnityEngine.Debug.LogFormat("UpWrite curWrite = {0}  retr = {1}", curWrite, length);
+
+            return 0;
+        }
+
+        private int GetPtr(ref IntPtr unmanaged_ptr)
+        {
+            int length = 0;
+            if (listPtr.Count <= 0)
+            {
+                return -1;
+            }
+            unmanaged_ptr = listPtr[0].myP;
+            length = listPtr[0].MyLength;
+            listPtr.RemoveAt(0);
+            return length;
+        }
+
+        private void OnWrite()
+        {
+            testB();
+
+            //testA();
+
+        }
+
+        private void testA()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+
+            while (true)
+            {
+                if (curWrite >= writeNum)
+                {
+                    break;
+                }
+
+                WriterFile(content);//3394.8485
+                curWrite++;
+            }
+
+
+            sw.Stop();
+            TimeSpan swTime = sw.Elapsed;
+
+            UnityEngine.Debug.LogFormat("TotalMilliseconds = {0}", swTime.TotalMilliseconds);
+        }
+
+        private void testB()
+        {
+            Stopwatch swB = new Stopwatch();
+            swB.Start();
+
+
+            while (true)
+            {
+                if (curWrite >= writeNum)
+                {
+                    break;
+                }
+
+                WriterFileB(content);//3394.8485
+                curWrite++;
+            }
+
+
+            swB.Stop();
+            TimeSpan swTimeB = swB.Elapsed;
+
+            UnityEngine.Debug.LogFormat("TotalMilliseconds BBB = {0}", swTimeB.TotalMilliseconds);
+        }
+
+        private void WriterFileB(byte[] content)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.Write))
+            {
+                fs.Write(content, 0, content.Length);
+            }
+
+            //UnityEngine.Debug.Log("Write done BBB ！");
+
+        }
+        private void WriterFile(byte[] content)
+        {
+            FileStream sw;
+
+            if (!file_info.Exists)
+            {
+                sw = file_info.Create();//创建一个用于写入 UTF-8 编码的文本  
+                //UnityEngine.Debug.Log("文件创建成功！");
+            }
+            else
+            {
+                sw = file_info.Open(FileMode.Append, FileAccess.Write, FileShare.Write);//打开现有 UTF-8 编码文本文件以进行读取  
+                //UnityEngine.Debug.Log("OpenWrite！");
+            }
+            sw.Write(content, 0, content.Length);
+            sw.Close();
+            sw.Dispose();//文件流释放  
+
+            //UnityEngine.Debug.Log("Write done ！");
+
+        }
+
+
+
+    }
+}
